00883289f6a6e74ed617eb8342b56abddd3c7e08
Doug <rdcassin@gmail.com>
2025-01-14 01:39:39 -0600

    added font-size and line-height. started ruler

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -16,8 +16,12 @@ import TextStyle from "@tiptap/extension-text-style";
 import Highlight from "@tiptap/extension-highlight";
 import { Color } from "@tiptap/extension-color";
 import Link from "@tiptap/extension-link";
+import TextAlign from "@tiptap/extension-text-align";
 
 import { useEditorStore } from "@/store/use-editor-store";
+import { FontSizeExtension } from "@/extensions/font-size";
+import { LineHeightExtension } from "@/extensions/line-height";
+import { Ruler } from "./ruler";
 
 export const Editor = () => {
   const { setEditor } = useEditorStore();
@@ -56,13 +60,16 @@ export const Editor = () => {
     },
     extensions: [
       StarterKit,
+      LineHeightExtension,
+      FontSizeExtension,
+      TextAlign.configure({ types: ["heading", "paragraph"] }),
       Link.configure({
         openOnClick: false,
         autolink: true,
         defaultProtocol: "https",
       }),
       Color,
-      Highlight.configure({ multicolor: true}),
+      Highlight.configure({ multicolor: true }),
       TextStyle,
       FontFamily,
       Underline,
@@ -97,6 +104,7 @@ export const Editor = () => {
 
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <Ruler />
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
